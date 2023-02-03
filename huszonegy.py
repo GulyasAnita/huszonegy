49e31c0aa39a720482d3ce303e2b0bde35b8c2b2
GulyasAnita <anitagulyas72@gmail.com>
2023-02-03 09:42:54 +0100

    pontszamitas

--- huszonegy.py
+++ huszonegy.py
@@ -1,9 +1,26 @@
 #megoldás
-def eredmeny(jatekos_pont,gep_pont):
-    if jatekos_pont > 21:
+jatekos_lapok = [2, 3, 4]
+gep_lapok = [10, 4, 10]
+
+
+def eredmeny(jatekos_lapok: [int], gep_lapok: [int]):
+    jatekos_lapok:int = pontszamitas(jatekos_lapok)
+    gep_lapok:int = pontszamitas(gep_lapok)
+    if jatekos_lapok > 21:
         print("vesztett")
-    elif gep_pont > 21:
+    elif gep_lapok > 21:
         print("vesztett")
     else:
         print("Mindketten vesztettek")
+
+def pontszamitas(lapok : [int]):
+    osszeg = 0
+    for i in range(len(lapok)):
+        osszeg = osszeg +lapok[i]
+    return osszeg
+
+
+
+
+
 #esetek
