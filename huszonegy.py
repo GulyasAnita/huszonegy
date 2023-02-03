b4f137618dd0a18bcc72a68f05d6cab5ecafd7f4
GulyasAnita <anitagulyas72@gmail.com>
2023-02-03 12:25:09 +0100

    vesztettTeszt

--- huszonegy.py
+++ huszonegy.py
@@ -1,18 +1,18 @@
 #megoldás
-jatekos_lapok = [2, 3, 4]
-gep_lapok = [10, 4, 10]
 
 
 def eredmeny(jatekos_lapok: [int], gep_lapok: [int]):
+    szoveg = ""
     jatekos_lapok:int = pontszamitas(jatekos_lapok)
     gep_lapok:int = pontszamitas(gep_lapok)
-    if jatekos_lapok > 21:
-        print("vesztett")
-    elif gep_lapok > 21:
-        print("vesztett")
-    else:
-        print("Mindketten vesztettek")
 
+    if jatekos_lapok > 21:
+        szoveg ="Játékos vesztett"
+    elif gep_lapok > 21:
+        szoveg="Gép vesztett"
+    else:
+        szoveg = "mindketten vesztettek"
+    return szoveg
 def pontszamitas(lapok : [int]):
     osszeg = 0
     for i in range(len(lapok)):
@@ -20,7 +20,20 @@ def pontszamitas(lapok : [int]):
     return osszeg
 
 
+def jatekosVesztettTeszt():
 
+    jatekosLista = [6, 4, 8, 9]
+    gepLista = [6, 4, 11]
+    kapottEredmeny = eredmeny(jatekosLista, gepLista)
+    vartEredmeny = "Játékos vesztett"
 
+    if kapottEredmeny == vartEredmeny:
+        print("Teszt sikeres. ")
+    else:
+        print("Teszt megbukott.")
 
+def tesztek():
+    jatekosVesztettTeszt()
+
+tesztek()
 #esetek
